1d0d04175b2eff8a97066444762d55df858d766f
surya <suryagsaputra@gmail.com>
2018-12-09 01:15:35 +0700

    initiate amazon sdk

--- app/src/main/java/project/sabil/schedulin/MainActivity.kt
+++ app/src/main/java/project/sabil/schedulin/MainActivity.kt
@@ -2,11 +2,19 @@ package project.sabil.schedulin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.amazonaws.mobile.client.AWSMobileClient
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        AWSMobileClient.getInstance().initialize(this) {
+            Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!")
+        }.execute()
+
     }
 }
