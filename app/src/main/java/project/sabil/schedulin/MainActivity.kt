303f4f2afc93a6613fbf22dbb749a2813ac49e74
surya <suryagsaputra@gmail.com>
2019-01-07 17:31:29 +0700

    separate code to method

--- app/src/main/java/project/sabil/schedulin/MainActivity.kt
+++ app/src/main/java/project/sabil/schedulin/MainActivity.kt
@@ -1,24 +1,21 @@
 package project.sabil.schedulin
 
-import android.util.Log
-import com.amazonaws.mobile.client.AWSMobileClient
 import com.trello.navi2.Event
-import com.trello.navi2.component.support.NaviAppCompatActivity
 import com.trello.navi2.rx.RxNavi
 import io.reactivex.android.schedulers.AndroidSchedulers
+import project.sabil.schedulin.base.BaseActivity
 
-class MainActivity : NaviAppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     init {
+        initLayout()
+    }
+
+    private fun initLayout() {
         RxNavi
                 .observe(this, Event.CREATE)
                 .observeOn(AndroidSchedulers.mainThread())
                 .takeUntil(RxNavi.observe(this, Event.DESTROY))
-                .subscribe {
-                    setContentView(R.layout.login_activity)
-                    AWSMobileClient.getInstance().initialize(this) {
-                        Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!")
-                    }.execute()
-                }
+                .subscribe { setContentView(R.layout.login_activity) }
     }
 }
