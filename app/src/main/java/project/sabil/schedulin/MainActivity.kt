4c503f2485c5401ee3ca0ce292870a816362b7a6
surya <suryagsaputra@gmail.com>
2019-01-07 19:30:46 +0700

    add main activity

--- app/src/main/java/project/sabil/schedulin/MainActivity.kt
+++ app/src/main/java/project/sabil/schedulin/MainActivity.kt
@@ -1,21 +1,10 @@
 package project.sabil.schedulin
 
-import com.trello.navi2.Event
-import com.trello.navi2.rx.RxNavi
-import io.reactivex.android.schedulers.AndroidSchedulers
 import project.sabil.schedulin.base.BaseActivity
 
 class MainActivity : BaseActivity() {
 
     init {
-        initLayout()
-    }
-
-    private fun initLayout() {
-        RxNavi
-                .observe(this, Event.CREATE)
-                .observeOn(AndroidSchedulers.mainThread())
-                .takeUntil(RxNavi.observe(this, Event.DESTROY))
-                .subscribe { setContentView(R.layout.login_activity) }
+        initLayout(R.layout.activity_main)
     }
 }
