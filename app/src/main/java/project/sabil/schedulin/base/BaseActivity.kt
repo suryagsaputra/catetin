403b82d82f2ec28efb9a4ed72cb60a3567a5069f
surya <suryagsaputra@gmail.com>
2019-01-07 19:31:24 +0700

    add base init layout

--- app/src/main/java/project/sabil/schedulin/base/BaseActivity.kt
+++ app/src/main/java/project/sabil/schedulin/base/BaseActivity.kt
@@ -1,7 +1,18 @@
 package project.sabil.schedulin.base
 
+import com.trello.navi2.Event
 import com.trello.navi2.component.support.NaviAppCompatActivity
+import com.trello.navi2.rx.RxNavi
+import io.reactivex.android.schedulers.AndroidSchedulers
 
 abstract class BaseActivity : NaviAppCompatActivity() {
+
+    protected fun initLayout(view: Int) {
+        RxNavi
+                .observe(this, Event.CREATE)
+                .observeOn(AndroidSchedulers.mainThread())
+                .takeUntil(RxNavi.observe(this, Event.DESTROY))
+                .subscribe { setContentView(view) }
+    }
 
 }
