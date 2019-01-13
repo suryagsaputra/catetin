d943ecbf694f70682fd9a91b32659c16a5d98c70
surya <suryagsaputra@gmail.com>
2019-01-13 15:22:12 +0700

    adding main activity

--- app/src/main/java/project/sabil/schedulin/MainActivity.kt
+++ app/src/main/java/project/sabil/schedulin/MainActivity.kt
@@ -1,10 +1,38 @@
 package project.sabil.schedulin
 
+import com.trello.navi2.Event
+import com.trello.navi2.rx.RxNavi
+import io.reactivex.android.schedulers.AndroidSchedulers
+import kotlinx.android.synthetic.main.activity_main.*
 import project.sabil.schedulin.base.BaseActivity
 
 class MainActivity : BaseActivity() {
 
+    private val eventName: String by lazy {
+        intent.getStringExtra(INTENT_EVENT_NAME)
+    }
+
+    private val eventZone: String by lazy {
+        intent.getStringExtra(INTENT_EVENT_ZONE)
+    }
+
     init {
         initLayout(R.layout.activity_main)
+        initUsername()
+    }
+
+    private fun initUsername() {
+        RxNavi
+                .observe(this, Event.CREATE)
+                .observeOn(AndroidSchedulers.mainThread())
+                .takeUntil(RxNavi.observe(this, Event.DESTROY))
+                .subscribe {
+                    dashboard_username.text = "$eventName | $eventZone"
+                }
+    }
+
+    companion object {
+        const val INTENT_EVENT_NAME = "intent.event_name"
+        const val INTENT_EVENT_ZONE = "intent.event_zone"
     }
 }
