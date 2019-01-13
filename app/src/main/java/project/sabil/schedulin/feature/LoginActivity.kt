f65c2c0344fe98f98d808c8e5de2379d14dd6a04
surya <suryagsaputra@gmail.com>
2019-01-13 15:23:14 +0700

    update intent to CreateEventActivity

--- app/src/main/java/project/sabil/schedulin/feature/LoginActivity.kt
+++ app/src/main/java/project/sabil/schedulin/feature/LoginActivity.kt
@@ -35,10 +35,7 @@ class LoginActivity : BaseActivity() {
     }
 
     private fun startMainActivity() {
-        val intent = Intent(this, MainActivity::class.java)
-                .apply {
-                    flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                }
+        val intent = Intent(this, CreateEventActivity::class.java)
         startActivity(intent)
     }
 }
