6dc1a9bfb99c8db9a38240f9cdfd74cbcc5dc43f
surya <suryagsaputra@gmail.com>
2019-03-23 01:56:43 +0700

    adding id listener

--- app/src/main/java/project/sabil/schedulin/MainActivity.kt
+++ app/src/main/java/project/sabil/schedulin/MainActivity.kt
@@ -1,10 +1,12 @@
 package project.sabil.schedulin
 
+import com.jakewharton.rxbinding3.widget.textChanges
 import com.trello.navi2.Event
 import com.trello.navi2.rx.RxNavi
 import io.reactivex.android.schedulers.AndroidSchedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import project.sabil.schedulin.base.BaseActivity
+import java.util.concurrent.TimeUnit
 
 class MainActivity : BaseActivity() {
 
@@ -19,6 +21,7 @@ class MainActivity : BaseActivity() {
     init {
         initLayout(R.layout.activity_main)
         initUsername()
+        initUserIdListener()
     }
 
     private fun initUsername() {
@@ -28,6 +31,18 @@ class MainActivity : BaseActivity() {
                 .takeUntil(RxNavi.observe(this, Event.DESTROY))
                 .subscribe {
                     dashboard_username.text = "$eventName | $eventZone"
+                }
+    }
+
+    private fun initUserIdListener() {
+        RxNavi
+                .observe(this, Event.CREATE)
+                .observeOn(AndroidSchedulers.mainThread())
+                .flatMap { dashboard_user_id.textChanges() }
+                .timeout(1, TimeUnit.HOURS)
+                .takeUntil(RxNavi.observe(this, Event.DESTROY))
+                .subscribe {
+                    dashboard_username.text = it.toString()
                 }
     }
 
